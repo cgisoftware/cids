e1d4719a2af3a29e4fefb5ad2c3ef8a1063f8f16
Vinícius Boscardin <boscardinvinicius@gmail.com>
2023-12-13 17:42:19 -0300

    :recycle: refactor: altera snackbar para composable

--- cids/lib/components/CgiSnackbar.vue
+++ cids/lib/components/CgiSnackbar.vue
@@ -1,15 +1,15 @@
 <template>
   <v-snackbar
-    :color="snackbar.color"
-    v-model="snackbar.snackbar"
-    :location="snackbar.position"
+    :color="snackbar.snackbarState.color"
+    v-model="snackbar.snackbarState.snackbar"
+    :location="snackbar.snackbarState.position"
     multi-line
-    :timeout="snackbar.timeout"
+    :timeout="snackbar.snackbarState.timeout"
   >
-    <div v-html="snackbar.message"></div>
+    <div v-html="snackbar.snackbarState.message"></div>
     <template v-slot:actions>
       <v-btn
-        v-show="snackbar.actions.accept"
+        v-show="snackbar.snackbarState.actions.accept"
         text
         color="white"
         @click="snackbar.accept()"
@@ -17,7 +17,7 @@
         Sim
       </v-btn>
       <v-btn
-        v-show="snackbar.actions.accept || snackbar.actions.show"
+        v-show="snackbar.snackbarState.actions.accept || snackbar.snackbarState.actions.show"
         color="white"
         @click="snackbar.decline()"
         icon="mdi-close"
@@ -27,6 +27,7 @@
   </v-snackbar>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { useSnackbar } from "../composable/CgiSnackbar.js";
+const snackbar = useSnackbar();
 </script>

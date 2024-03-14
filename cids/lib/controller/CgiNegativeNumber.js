098ebda737f6e798938fadc21ed884fb216a4f56
João Pedro Ficanha <jpfficanha@gmail.com>
2024-03-14 08:34:51 -0300

    :recycle: refactor(CgiNegativeNumber): altera para funcionar no vue3

--- cids/lib/controller/CgiNegativeNumber.js
+++ cids/lib/controller/CgiNegativeNumber.js
@@ -3,38 +3,39 @@ import { getInput } from "./handler/GetInput";
 let callback;
 
 function containsOnlyNumbers(str) {
-    if (str === "") {
-        return true
-    }
-    return /^\d+$/.test(str);
+  if (str === "") {
+    return true;
+  }
+  return /^\d+$/.test(str);
 }
 
 export default {
-    install(Vue) {
-        Vue.directive('cgi-negative-number', {
-            bind(el, binding) {
-                el = getInput(el)
-                const habilitado = binding.value ?? true;
+  install(Vue) {
+    Vue.directive("cgi-negative-number", {
+      mounted(el, binding) {
+        el = getInput(el);
+        const habilitado = binding.value ?? true;
 
-                if (habilitado) {
-                    callback = (e) => {
-                        if (!containsOnlyNumbers(e.target.value)) {
-                            const value = e.target.value.replace(/\D+/g, '')
-                            e.target.value = value ? parseInt(value) : null;
+        if (habilitado) {
+          callback = (e) => {
+            if (!containsOnlyNumbers(e.target.value)) {
+              const value = e.target.value.replace(/\D+/g, "");
+              e.target.value = value ? parseInt(value) : null;
 
-                            const event = new Event("input", { "bubbles": true, "cancelable": true });
-                            e.target.dispatchEvent(event);
-                        }
-                        // if ( || (e.data === "-" || e.data === "+")) {
-
-                        // }
-                    };
-                    el.addEventListener('input', callback);
-                }
-            },
-            unbind(el) {
-                el.removeEventListener('input', callback);
+              const event = new Event("input", {
+                bubbles: true,
+                cancelable: true,
+              });
+              e.target.dispatchEvent(event);
             }
-        });
-    }
+          };
+
+          el.addEventListener("input", callback);
+        }
+      },
+      beforeUnmount(el) {
+        el.removeEventListener("input", callback);
+      },
+    });
+  },
 };

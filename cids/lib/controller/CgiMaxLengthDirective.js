ced7290e0da4091e682474cd41de4a484b905c0e
João Pedro Ficanha <jpfficanha@gmail.com>
2024-03-14 08:35:09 -0300

    :recycle: refactor(CgiMaxLength): altera para funcionar no vue3

--- cids/lib/controller/CgiMaxLengthDirective.js
+++ cids/lib/controller/CgiMaxLengthDirective.js
@@ -2,28 +2,31 @@ import { getInput } from "./handler/GetInput";
 
 let callback;
 
-
 export default {
-    install(Vue) {
-        Vue.directive('cgi-max-length', {
-            bind(el, binding) {
-                el = getInput(el)
+  install(Vue) {
+    Vue.directive("cgi-max-length", {
+      mounted(el, binding) {
+        el = getInput(el);
 
-                const maxLength = binding.expression;
-                callback = (e) => {
-                    if (e.target.value.length > maxLength) {
-                        e.target.value = e.target.value.substr(0, maxLength);
+        const maxLength = binding.value;
 
-                        const event = new Event("input", { "bubbles": true, "cancelable": true });
-                        // event.dispatchEvent('input', true, true);
-                        e.target.dispatchEvent(event);
-                    }
-                };
-                el.addEventListener('input', callback);
-            },
-            unbind(el) {
-                el.removeEventListener('input', callback);
-            }
-        });
-    }
+        callback = (e) => {
+          if (e.target.value.length > maxLength) {
+            e.target.value = e.target.value.substr(0, maxLength);
+
+            const event = new Event("input", {
+              bubbles: true,
+              cancelable: true,
+            });
+
+            e.target.dispatchEvent(event);
+          }
+        };
+        el.addEventListener("input", callback);
+      },
+      beforeUnmount(el) {
+        el.removeEventListener("input", callback);
+      },
+    });
+  },
 };

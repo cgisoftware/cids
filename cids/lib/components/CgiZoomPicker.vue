52613da63e744f36f4c9d520c999247bef7fc119
João Pedro Ficanha <jpfficanha@gmail.com>
2024-04-24 11:58:28 -0300

    :sparkles: feat(CGIZoomPicker): adiciona emit do botão clear

--- cids/lib/components/CgiZoomPicker.vue
+++ cids/lib/components/CgiZoomPicker.vue
@@ -5,69 +5,75 @@
         <v-text-field
           ref="textfield"
           v-if="!custom"
+          v-model:model-value="valor"
+          v-cgi-negative-number="tipo === 'number'"
           :label="nome"
           :dense="compacto"
           :rules="regras"
-          v-model:model-value="valor"
           :disabled="desabilitado"
           :readonly="somenteLeitura"
-          @click:prepend-inner="chamaZoom"
-          @click:append-inner="clear"
-          prepend-inner-icon="mdi-database-search-outline"
-          append-inner-icon="mdi-close"
-          @blur="blur"
           :loading="loading"
-          v-cgi-negative-number="tipo === 'number'"
+          @blur="blur"
         >
+          <template v-slot:append-inner>
+            <v-icon tabindex="-1" @click="clear"> mdi-close </v-icon>
+          </template>
+
+          <template v-slot:prepend-inner>
+            <v-icon tabindex="-1" @click="chamaZoom">
+              mdi-database-search-outline
+            </v-icon>
+          </template>
         </v-text-field>
       </v-col>
+
       <v-col v-if="formataValor">
         <v-text-field
-          tabindex="-1"
           v-if="!custom"
+          tabindex="-1"
           :dense="compacto"
           :value="descricao"
           readonly
-        >
-        </v-text-field>
+        />
       </v-col>
     </v-row>
 
-    <slot name="customcomp" v-bind:chamaZoom="chamaZoom"> </slot>
+    <slot name="customcomp" v-bind:chamaZoom="chamaZoom" />
 
     <v-dialog
       v-if="dialog"
+      height="608px"
+      class="pa-0"
+      :model-value="true"
+      :width="largura"
       persistent
       scrollable
-      overlay-opacity="0"
-      :model-value="true"
-      class="pa-0"
-      height="608px"
-      :width="largura"
     >
       <component
-        v-if="componenteZoom && !iframeUrl"
         ref="component"
+        v-if="componenteZoom && !iframeUrl"
+        :is="componenteZoom"
+        :registro="valor"
         @click:close="close"
         @cancelar-zoom="close"
         @confirmar-zoom="confirma"
-        v-bind:is="componenteZoom"
-        :registro="valor"
         @exporta-zoom="setaValor"
-      ></component>
+      />
+
       <v-card v-show="!componenteZoom && iframeUrl">
         <iframe
           :src="iframeUrl"
           width="100%"
           height="100%"
           style="border: white"
-        ></iframe>
+        />
       </v-card>
+
       <v-card v-if="!componenteZoom && !iframeUrl" height="500">
-        <v-card-title>
-          <v-spacer></v-spacer>
+        <v-card-title class="d-flex align-center justify-end">
           <v-icon @click="close">mdi-close</v-icon>
         </v-card-title>
+
         <v-card-text class="d-flex justify-center align-center">
           <div class="font-weight-bold red--text display-1">
             Zoom não disponível
@@ -82,9 +88,10 @@
 import { computed, nextTick, onMounted, ref, useSlots, watch } from "vue";
 import { debounce, publisher, subscriber } from "../util";
 
-const emits = defineEmits([
-  "update:modelValue",
+const emit = defineEmits([
+  "update:model-value",
   "change",
+  "clear",
   "perde-foco",
   "cancelar-zoom",
   "confirmar-zoom",
@@ -170,7 +177,7 @@ watch(valor, () => {
   if (props.formataValor) {
     debounceSearch();
   } else {
-    emits("update:modelValue", valor.value);
+    emit("update:model-value", valor.value);
   }
 });
 
@@ -184,6 +191,7 @@ watch(
     ) {
       descricao.value = null;
     }
+
     valor.value = props.modelValue;
   }
 );
@@ -192,7 +200,8 @@ const updateSearch = async () => {
   if (valor.value && props.aoDigitar) {
     descricao.value = await props.aoDigitar(valor.value);
   }
-  emits("update:modelValue", valor.value);
+
+  emit("update:model-value", valor.value);
 };
 
 const debounceSearch = debounce(updateSearch, 500);
@@ -201,11 +210,10 @@ onMounted(async () => {
   if (valor.value && props.aoDigitar) {
     descricao.value = await props.aoDigitar(valor);
   }
+
   componenteZoom.value = await renderComponente();
 
-  if (!subscriber) {
-    return;
-  }
+  if (!subscriber) return;
 
   subscriber(props.zoom).listen("getParams", loadParams);
   subscriber(props.zoom).exportZoom(setaValor);
@@ -230,6 +238,7 @@ const renderComponente = async () => {
     return null;
   }
 };
+
 const setaValor = async (item) => {
   if (!custom.value) {
     valor.value = item[props.chave];
@@ -237,6 +246,7 @@ const setaValor = async (item) => {
     if (props.formataValor) {
       descricao.value = item[props.campoValorFormatado];
     }
+
     dialog.value = false;
 
     if (valor.value && props.aoDigitar) {
@@ -244,25 +254,24 @@ const setaValor = async (item) => {
     }
 
     setFocus();
-    emits("change", item);
+    emit("change", item);
   }
 };
+
 const chamaZoom = async () => {
-  if (props.desabilitado || props.somenteLeitura) {
-    return;
-  }
+  if (props.desabilitado || props.somenteLeitura) return;
 
   dialog.value = true;
   await new Promise((resolver) => setTimeout(resolver, 100));
+
   if (componenteZoom.value && !iframeUrl.value) {
     chamaZoomComponente();
     return;
   }
 };
+
 const loadParams = () => {
-  if (!publisher) {
-    return;
-  }
+  if (!publisher) return;
 
   publisher(props.zoom).send("dialogZoom", true);
 
@@ -284,6 +293,7 @@ const loadParams = () => {
     valor.value && valor.value !== 0 ? valor.value.toString() : null
   );
 };
+
 const chamaZoomComponente = async () => {
   component.value.controller.dialogZoom.value = true;
 
@@ -309,31 +319,41 @@ const chamaZoomComponente = async () => {
     component.value.controller.queryZoom(props.params);
   }
 };
-const blur = () => {
-  emits("perde-foco", valor.value);
+
+const blur = async () => {
+  await nextTick();
+
+  if (dialog.value) return;
+
+  emit("perde-foco", valor.value);
 };
+
 const close = () => {
   dialog.value = false;
   setFocus();
-  emits("cancelar-zoom");
+  emit("cancelar-zoom");
 };
+
 const confirma = () => {
   dialog.value = false;
   setFocus();
-  emits("confirmar-zoom");
+  emit("confirmar-zoom");
 };
+
 const clear = () => {
   if (props.desabilitado || props.somenteLeitura) {
     return;
   }
+
   valor.value = null;
   descricao.value = null;
+  emit("clear");
 };
 
-const setFocus = () => {
-  nextTick(() => {
-    textfield.value.focus();
-  });
+const setFocus = async () => {
+  await nextTick();
+
+  textfield.value.focus();
 };
 </script>
 

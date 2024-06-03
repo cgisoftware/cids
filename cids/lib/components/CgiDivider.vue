7830e73870e55d79ba11e39c9a899dd2496c7f2f
JacksonSantin <jackdhanyelsn@gmail.com>
2024-06-03 15:30:51 -0300

    :sparkles: Feat(CGIDivider): Remove required e adiciona styles

--- cids/lib/components/CgiDivider.vue
+++ cids/lib/components/CgiDivider.vue
@@ -1,13 +1,10 @@
 <template>
   <v-card
-    class="my-6"
+    class="my-6 custom-divider-light custom-divider-dark"
     variant="outlined"
   >
     <div class="d-flex justify-center">
-      <v-sheet
-        position="fixed"
-        style="margin-top: -12px;"
-      >
+      <v-sheet position="fixed" style="margin-top: -12px">
         <div class="mx-2">{{ titulo }}</div>
       </v-sheet>
     </div>
@@ -22,7 +19,24 @@
 defineProps({
   titulo: {
     type: String,
-    required: true,
   },
 });
 </script>
+
+<style scoped>
+.v-theme--cgi .custom-divider-light {
+  color: #ababab;
+}
+
+.v-theme--cgi .custom-divider-light ::v-deep * {
+  color: initial;
+}
+
+.v-theme--dark .custom-divider-dark {
+  color: #757575;
+}
+
+.v-theme--dark .custom-divider-dark ::v-deep * {
+  color: initial;
+}
+</style>

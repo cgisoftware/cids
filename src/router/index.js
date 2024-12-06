6a331244949a1147d17767b319fcf5cba97b5d15
Gabriel Brocco de Oliveira <gabsbrocco@gmail.com>
2024-12-06 09:12:29 -0300

    :sparkles: feat(router): Implementa scrollBehavior para voltar ao topo da tela quando troca de menu

--- src/router/index.js
+++ src/router/index.js
@@ -84,6 +84,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
